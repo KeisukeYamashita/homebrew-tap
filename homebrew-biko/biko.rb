13dd36773b60e45aa7ceb8069647f1a2be9d147d
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-01 01:55:44 +0900

    Brew formula update for biko version v0.0.18

--- homebrew-biko/biko.rb
+++ homebrew-biko/biko.rb
@@ -2,16 +2,16 @@
 class Biko < Formula
   desc "CLI tool to jump to your browser directly"
   homepage "https://github.com/KeisukeYamashita/biko"
-  version "0.0.17"
+  version "0.0.18"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/KeisukeYamashita/biko/releases/v0.0.17/biko_0.0.17_darwin_amd64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "c18ece31ca46424e260b21a1c5a28978b78ebee487940131d0731a6584731f6c"
+    url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.18/biko_0.0.18_darwin_amd64.tar.gz"
+    sha256 "7970e34ad7d5dbdd4240d98e37d607c578df22d97e0226c7e702285ff0ef42dd"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "http://github.com/KeisukeYamashita/biko/releases/v0.0.17/biko_0.0.17_linux_amd64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "78cc78b359d59d983a960a6dbc42cf567996d4bcbc433cd3db10a0dd1a03a1d9"
+    url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.18/biko_0.0.18_linux_amd64.tar.gz"
+    sha256 "f27180f7556f0bcf245ef8817ac3bf3dd01f52df48502d97cea12b396d30340e"
   end
 
   def install
