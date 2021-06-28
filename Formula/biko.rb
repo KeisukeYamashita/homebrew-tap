0809ceebe8cb8108dbec81f2c6319fe025dbb14f
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-28 18:05:51 +0900

    Brew formula update for biko version v0.0.21

--- Formula/biko.rb
+++ Formula/biko.rb
@@ -5,28 +5,28 @@
 class Biko < Formula
   desc "CLI tool to jump to your browser directly"
   homepage "https://github.com/KeisukeYamashita/biko"
-  version "0.0.20"
+  version "0.0.21"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.20/biko_0.0.20_darwin_amd64.tar.gz"
-      sha256 "796347ee78001827d5ae0546a728c27a6589a9743a8ebdc59923649f92a165e7"
+      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.21/biko_0.0.21_darwin_amd64.tar.gz"
+      sha256 "fd0bcd06f2a8e0a25c95d3fab54b6b4e6be7ddabdffc1cbcca121bfe59147832"
     end
     if Hardware::CPU.arm?
-      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.20/biko_0.0.20_darwin_arm64.tar.gz"
-      sha256 "338670da7da9bcd3dd92009db3165f347686275874ea826d4a851216b23d33b3"
+      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.21/biko_0.0.21_darwin_arm64.tar.gz"
+      sha256 "d4f9734c25cc69838f664db58fcf4913f5c9766401b3ac1066452af0fb5ade8d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.20/biko_0.0.20_linux_amd64.tar.gz"
-      sha256 "072982538ab331b14cc4d4d4eb7a4a3df822720f68c2265774eea921332cc308"
+      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.21/biko_0.0.21_linux_amd64.tar.gz"
+      sha256 "59224686eb27bbbe4ac0bf2dc9e005ac7f9de44497736c7c847aa7da7a29bc25"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.20/biko_0.0.20_linux_arm64.tar.gz"
-      sha256 "8c7033a47d759dc66f0a20ff00ae491e978dcc95bf14b46e334d068fc0a35c56"
+      url "http://github.com/KeisukeYamashita/biko/releases/download/v0.0.21/biko_0.0.21_linux_arm64.tar.gz"
+      sha256 "becd923c25fd71bc6bf2c1df710192066471835d72c9f72887b4a0bf6a79fd1b"
     end
   end
 
